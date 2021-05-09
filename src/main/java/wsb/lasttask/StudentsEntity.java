733b984f05e3b580f15d395386ea6cef60f258ef
Karol Cissewski <cissewskikarol@gmail.com>
2021-05-09 20:03:03 +0200

    Use lombok

--- src/main/java/wsb/lasttask/StudentsEntity.java
+++ src/main/java/wsb/lasttask/StudentsEntity.java
@@ -1,5 +1,6 @@
 package wsb.lasttask;
 
+import lombok.Data;
 import lombok.ToString;
 
 import javax.persistence.*;
@@ -8,66 +9,22 @@ import java.util.Objects;
 @Entity
 @Table(name = "students", schema = "public", catalog = "oop2020")
 @NamedQuery(name = "StudentsEntity.byLastName", query = "select s from StudentsEntity s where lastName like ?1")
-@ToString
+@Data
 public class StudentsEntity {
-    private String firstName;
-    private String lastName;
-    private Integer age;
+
+    @Id
+    @Column(name = "id")
     private Integer id;
 
     @Basic
     @Column(name = "first_name")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    private String firstName;
 
     @Basic
     @Column(name = "last_name")
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    private String lastName;
 
     @Basic
     @Column(name = "age")
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Id
-    @Column(name = "id")
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StudentsEntity that = (StudentsEntity) o;
-        return Objects.equals(firstName, that.firstName) &&
-                Objects.equals(lastName, that.lastName) &&
-                Objects.equals(age, that.age) &&
-                Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, age, id);
-    }
+    private Integer age;
 }
